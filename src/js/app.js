618eedf03ef7ae9b1597a9e48f2a98aad6123228
Alexey Bondarenko <alexeybondarenko@me.com>
2016-08-15 23:45:14 +0300

    fix mobile scroll

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,13 @@
   window.appName = '<%= appname %>';
 
   //new WOW().init();
-  var s = skrollr.init();
+  var s = skrollr.init({
+    forceHeight: false,
+    mobileCheck: function() {
+      //hack - forces mobile version to be off
+      return false;
+    }
+  });
 
 
 })();
